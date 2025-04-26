f8c4ebeb5286fd2359c7f5465c614b081370f748
Tariq Sherazi <112605197+tariq1010@users.noreply.github.com>
2025-04-26 21:43:39 +0500

    remove post validation in gateway

--- gateway/src/config/validation.ts
+++ gateway/src/config/validation.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 export const configValidationSchema = Joi.object({
   USER_SERVICE_PORT: Joi.number().required(),
   USER_SERVICE_HOST: Joi.string().required(),
-  POST_SERVICE_PORT: Joi.number().required(),
-  POST_SERVICE_HOST: Joi.string().required(),
+  // POST_SERVICE_PORT: Joi.number().required(),
+  // POST_SERVICE_HOST: Joi.string().required(),
   GATEWAY_PORT: Joi.number().required(),
 });
